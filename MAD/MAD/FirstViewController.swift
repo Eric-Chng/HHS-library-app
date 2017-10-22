7d3740532f588a485bb3f4cb154a85eb095a4bd1
David McAllister <davidrmc2@gmail.com>
2017-10-21 22:22:30 -0700

    ScrollingHomePage

--- MAD/MAD/FirstViewController.swift
+++ MAD/MAD/FirstViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var navBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //navBar.title = "Home"
+        //navigationItem.title="Home"
+        setupNavigationBarItems()
+    navigationController?.navigationBar.prefersLargeTitles = true
         // Do any additional setup after loading the view, typically from a nib.\
         //eric cheng
     }
+    
+    private func setupNavigationBarItems()
+    {
+        navigationItem.title = "Home"
+        
+    }
+    
+    
+    @IBOutlet weak var UIScrollView: UIScrollView!
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
