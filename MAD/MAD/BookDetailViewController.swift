83a7cbd803121ae285864c73f37b417d7b00249c
David McAllister <davidrmc2@gmail.com>
2017-11-09 21:14:08 -0800

    d

--- MAD/MAD/BookDetailViewController.swift
+++ MAD/MAD/BookDetailViewController.swift
@@ -31,7 +31,19 @@ class BookDetailViewController : UIViewController {
         authorLabel?.text = "Colson Whitehead"
         descLabel?.text = "Description: " + "the best description"
         self.BookCoverImage.image = #imageLiteral(resourceName: "sampleCover")
+        //BookCoverImage.clipsToBounds
+        
+        
+        /*
+         Root Stack View.Leading = Superview.LeadingMargin
+         Root Stack View.Trailing = Superview.TrailingMargin
+         Root Stack View.Top = Top Layout Guide.Bottom + 20.0
+         Bottom Layout Guide.Top = Root Stack View.Bottom + 20.0
+         Image View.Height = Image View.Width
+         First Name Text Field.Width = Middle Name Text Field.Width
+         First Name Text Field.Width = Last Name Text Field.Width
 
+         */
         
     }
     
