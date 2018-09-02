79dab91e5c574bc89f70135422bf2d72d7509fa6
David McAllister <davidrmc2@gmail.com>
2018-09-01 19:25:11 -0700

    test

--- FUHSD Libraries/Pods/Target Support Files/Pods-FUHSD Libraries/Pods-FUHSD Libraries-frameworks.sh
+++ FUHSD Libraries/Pods/Target Support Files/Pods-FUHSD Libraries/Pods-FUHSD Libraries-frameworks.sh
@@ -144,7 +144,6 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BulletinBoard/BulletinBoard.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/CDCodabarView/CDCodabarView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CollectionKit/CollectionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMOAuth2/GTMOAuth2.framework"
@@ -162,7 +161,6 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/BulletinBoard/BulletinBoard.framework"
-  install_framework "${BUILT_PRODUCTS_DIR}/CDCodabarView/CDCodabarView.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/ChameleonFramework/ChameleonFramework.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/CollectionKit/CollectionKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GTMOAuth2/GTMOAuth2.framework"
